f33b33b7a8d28de7f40cc810fbe82a5788068080
fernandocabal <nando-097@hotmail.com>
2024-07-18 23:08:48 -0400

    Hasta aca llegamos hoy

--- connet/conexion.php
+++ connet/conexion.php
@@ -1,13 +1,13 @@
 <?php
-// $connect = mysqli_connect("localhost", "root", "", "lavadero", "3306");
-// if (!$connect) {
-//     die("NO SE CONECTÓ" . mysqli_connect_error());
-// }
-?>
-
-<?php
-$connect = mysqli_connect("bihq7cbc9dmtmxtcrxnb-mysql.services.clever-cloud.com", "u1pmsielszqljh5d", "ndqyGPJ4IMRxiyS5IzF1", "bihq7cbc9dmtmxtcrxnb", "3306");
+$connect = mysqli_connect("localhost", "root", "", "lavadero", "3306");
 if (!$connect) {
     die("NO SE CONECTÓ" . mysqli_connect_error());
 }
+?>
+
+<?php
+// $connect = mysqli_connect("bihq7cbc9dmtmxtcrxnb-mysql.services.clever-cloud.com", "u1pmsielszqljh5d", "ndqyGPJ4IMRxiyS5IzF1", "bihq7cbc9dmtmxtcrxnb", "3306");
+// if (!$connect) {
+//     die("NO SE CONECTÓ" . mysqli_connect_error());
+// }
 ?>
